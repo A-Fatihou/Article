b85aec7020cd6c3dd3d52884a277ffe89149fa6b
A-Fatihou <adamoufatihou@hotmail.com>
2023-10-02 16:53:40 +0200

    init

---
+++ pageArticle.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voici votre liste d'articles</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Voici votre liste d'articles</h1>
+    <div class="articles-container">
+        <?php include 'articles.php'; ?>
+    </div>
+</body>
+</html>
